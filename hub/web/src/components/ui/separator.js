3edeed167d0bb0188500f6e2f9df00c1cb804861
Will McDonald <wemcdonald@gmail.com>
2026-03-17 15:37:29 -0700

    chore: add hub/web/.gitignore, remove compiled artifacts from tracking

--- hub/web/src/components/ui/separator.js
+++
@@ -1,8 +0,0 @@
-"use client";
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Separator as SeparatorPrimitive } from "@base-ui/react/separator";
-import { cn } from "@/lib/utils";
-function Separator({ className, orientation = "horizontal", ...props }) {
-    return (_jsx(SeparatorPrimitive, { "data-slot": "separator", orientation: orientation, className: cn("shrink-0 bg-border data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch", className), ...props }));
-}
-export { Separator };
--- hub/web/src/main.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-ReactDOM.createRoot(document.getElementById("root")).render(_jsx(React.StrictMode, { children: _jsx("div", { className: "flex items-center justify-center min-h-screen bg-background text-foreground", children: _jsx("h1", { className: "text-2xl font-bold", children: "OmniDeck" }) }) }));
